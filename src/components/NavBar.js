ff566fe1f23282bdb5b7a6b0778e661c58fa5d98
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-11-28 01:25:17 -0500

    complted lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  
+  const linkElements = links.map(link=>{
+    return <a  key={link}href={`#${link}`}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,18 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectsElements = projects.map(project => {
+    return <ProjectItem key = {project.id} name={project.name} about = {project.about} technologies={project.technologies} ></ProjectItem>
+  })
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectsElements}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+
